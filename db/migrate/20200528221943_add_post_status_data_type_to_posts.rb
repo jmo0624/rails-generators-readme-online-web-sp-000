7b56a57d4dae37dc31b33e915575cd415cc8ba6e
Josh Morris <jmo0624@gmail.com>
2020-05-29 00:17:46 +0000

    Automatically backed up by Learn

--- db/migrate/20200528221943_add_post_status_data_type_to_posts.rb
+++ db/migrate/20200528221943_add_post_status_data_type_to_posts.rb
@@ -1,4 +1,4 @@
-class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration[5.0]
+class AddPostStatusDataTypeToPosts < ActiveRecord::Migration[5.0]
   def change
     change_column :posts, :post_status, :string
   end
